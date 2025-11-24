f480ccd71d7fffee420a3cead30841668cc219af
Vicente123456789021 <vicentedoomoreira@gmail.com>
2025-11-24 11:27:42 +0000

    criei tabela medico e paciente

--- Server/initialize_server.py
+++ Server/initialize_server.py
@@ -7,6 +7,8 @@ def start_check_use_db():
     cursor.execute("CREATE DATABASE IF NOT EXISTS hospytal_db")
     cursor.execute("USE hospytal_db")
 
+    cursor.execute("CREATE TABLE IF NOT EXISTS medico ( id INT AUTO_INCREMENT PRIMARY KEY ,nif CHAR(9) UNIQUE, nome VARCHAR(255) NOT NULL, idade INT NOT NULL, email VARCHAR(255) UNIQUE NOT NULL, senha VARCHAR(255) NOT NULL, especialidade VARCHAR(100) NOT NULL, salario DECIMAL(10,2) NOT NULL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS paciente ( id INT AUTO_INCREMENT PRIMARY KEY ,nif CHAR(9) UNIQUE, nome VARCHAR(255) NOT NULL, idade INT NOT NULL, email VARCHAR(255) UNIQUE NOT NULL, senha VARCHAR(255) NOT NULL, utente VARCHAR(50) NOT NULL)")
     cursor.execute("CREATE TABLE IF NOT EXISTS secretaria (id INT AUTO_INCREMENT PRIMARY KEY ,nif CHAR(9) UNIQUE, nome VARCHAR(255) NOT NULL, idade INT NOT NULL, email VARCHAR(255) UNIQUE NOT NULL, senha VARCHAR(255) NOT NULL, lugar_trabalho VARCHAR(100) NOT NULL, salario DECIMAL(10,2) NOT NULL)")
     cursor.execute("CREATE TABLE IF NOT EXISTS consulta (id INT AUTO_INCREMENT PRIMARY KEY, paciente_id CHAR(9) NOT NULL, medico_id CHAR(9) NOT NULL, data DATE NOT NULL, hora TIME NOT NULL, motivo VARCHAR(255), cancelado BOOLEAN DEFAULT FALSE, atendido BOOLEAN DEFAULT FALSE, FOREIGN KEY (paciente_id) REFERENCES paciente(nif) ON DELETE CASCADE, FOREIGN KEY (medico_id) REFERENCES medico(nif) ON DELETE CASCADE, UNIQUE (medico_id, data, hora))")
 
